6fa561ae2b7341b10f49a54220052e104fdc531a
shawonTalukder <shawontalukder.study@gmail.com>
2023-05-31 23:30:25 +0600

    [ Router setup ] home component is linked with the router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-import './assets/main.css'
+import './assets/main.css';
+import router from './router';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+//module scaffolding
+const app = createApp(App);
+
+//middlewares
+//add router
+app.use(router);
+
+//mount the app
+app.mount('#app');
