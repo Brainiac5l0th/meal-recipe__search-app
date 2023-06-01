2166136cf01e53305b35b71669425fd89a1f858b
shawonTalukder <shawontalukder.study@gmail.com>
2023-06-01 11:45:01 +0600

    search and filter by letters functionality added

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue"
+import MealList from "../views/MealList.vue"
 
 //defining all the routes
 const routes = [
@@ -7,6 +8,11 @@ const routes = [
         path: "/",
         name: "home",
         component: Home
+    },
+    {
+        path: "/letter/:letter",
+        name: "byLetter",
+        component: MealList
     }
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,22 @@
 import { computed } from 'vue';
 import store from '../app/store';
 
-    const meals = computed(() => store.state.meals)
+const meals = computed(() => store.state.meals)
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
 </script>
+
 <template>
-    {{ meals }}
+    <div class="flex p-8 justify-center">
+        <input type="text" class="rounded-md border-gray-500 font-semibold w-full md:w-3/4"
+            placeholder="search for meals" />
+    </div>
+
+    <div class="flex justify-center gap-2 mt-2 wrap">
+        <router-link :to="{
+            name: 'byLetter', params: { letter }
+        }" v-for="letter of letters" :key="letter">{{ letter
+}}</router-link>
+    </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
