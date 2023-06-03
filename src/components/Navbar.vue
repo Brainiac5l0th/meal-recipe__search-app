71f5b73eac23fa462bddad23600d8d25f2a45352
shawonTalukder <shawontalukder.study@gmail.com>
2023-06-03 19:26:20 +0600

    navbar hidden in mobile screen

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,7 @@
         <router-link :to="{ name: 'Home' }" class="inline-flex items-center font-semibold text-xl px-2 h-full">
             Meals
         </router-link>
-        <div class="flex md:gap-3">
+        <div class="flex md:gap-3 hidden md:inline-flex">
             <router-link :to="{ name: 'byName', params: name }"
                 class="px-2 inline-flex items-center hover:bg-purple-50">Search
                 Meals</router-link>
