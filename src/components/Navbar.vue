2bd0e64599cb0b5a6050c1f6a14832065609762d
shawonTalukder <shawontalukder.study@gmail.com>
2023-06-04 23:28:58 +0600

    make navbar bigger for [ up to medium screen ]

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,20 @@
 <script setup></script>
 <template>
-    <header class="flex justify-between sticky top-0 w-full h-16 lg:w-3/4 m-auto md:px-4 bg-gray-50 rounded-md z-50">
+    <header
+        class="flex justify-between sticky top-0 w-full h-16 lg:w-10/12 m-auto md:px-4 bg-gray-50 rounded-md z-50 shadow-md">
         <router-link :to="{ name: 'Home' }" class="inline-flex items-center font-semibold text-xl px-2 h-full">
             Meals
         </router-link>
         <div class="flex md:gap-3 hidden md:inline-flex">
-            <router-link :to="{ name: 'byName', params: name }"
-                class="px-2 inline-flex items-center hover:bg-purple-50">Search
-                Meals</router-link>
-            <router-link :to="{ name: 'byLetter' }" class="px-2 inline-flex items-center hover:bg-purple-50">Meals By
-                Letter</router-link>
-            <router-link :to="{ name: 'byIngrediants' }" class="px-2 inline-flex items-center hover:bg-purple-50">Meals By
-                Ingrediants</router-link>
+            <router-link :to="{ name: 'byName', params: name }" class="px-2 inline-flex items-center hover:bg-purple-50">
+                Search Meals
+            </router-link>
+            <router-link :to="{ name: 'byLetter' }" class="px-2 inline-flex items-center hover:bg-purple-50">
+                Meals By Letter
+            </router-link>
+            <router-link :to="{ name: 'byIngrediants' }" class="px-2 inline-flex items-center hover:bg-purple-50">
+                Meals By Ingrediants
+            </router-link>
         </div>
     </header>
 </template>
