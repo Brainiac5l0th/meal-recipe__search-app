a56391f0219c53bc7d06f45ca558d4c103ecb7af
shawonTalukder <shawontalukder.study@gmail.com>
2023-06-04 23:54:27 +0600

    new component for Ingrediants item

---
+++ src/components/IngrediantItem.vue
@@ -0,0 +1,11 @@
+<template>
+    <p
+        class="bg-slate-200 px-4 py-3 rounded hover:bg-slate-300 hover:shadow-md cursor-pointer text-center font-semibold text-wrap">
+        <slot></slot>
+    </p>
+</template>
+
+<script setup>
+</script>
+
+<style scoped></style>
