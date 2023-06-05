36acac0d99aa71269bc6266971ca04ffc0fcd771
shawonTalukder <shawontalukder.study@gmail.com>
2023-06-05 11:19:27 +0600

    showing all ingrediants for there.without description.

--- src/views/MealsByIngrediants.vue
+++ src/views/MealsByIngrediants.vue
@@ -4,8 +4,11 @@
     <div class="w-full md:w-3/4 m-auto px-2 mt-4">
         <div class="grid grid-cols-2 sm:grid-cols-4 xl:grid-cols-6 grid-wrap gap-2 md:gap-4 my-2 md:my-4">
             <template v-if="ingrediants.length > 0">
-                <IngrediantItem v-for="ingrediant of ingrediants" :key="ingrediant?.idIngredient">{{
-                    ingrediant.strIngredient }}
+
+                <IngrediantItem v-for="ingrediant of ingrediants" :key="ingrediant?.idIngredient">
+                    <router-link :to="{ name: 'ingrediants', params: { ingrediant: ingrediant.strIngredient } }">{{
+                        ingrediant.strIngredient }}
+                    </router-link>
                 </IngrediantItem>
             </template>
             <template v-else class="text-center font-bold text-md">Loading...</template>
