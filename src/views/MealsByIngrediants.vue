3d0c0e13e02100b83427ab9d56e14a3dc4e6664b
shawonTalukder <shawontalukder.study@gmail.com>
2023-06-05 10:04:23 +0600

    a lot to do!

--- src/views/MealsByIngrediants.vue
+++ src/views/MealsByIngrediants.vue
@@ -2,23 +2,29 @@
 
 <template>
     <div class="w-full md:w-3/4 m-auto px-2 mt-4">
-        <div class="grid grid-cols-2 sm:grid-cols-4 xl:grid-cols-6 grid-wrap gap-2 md:gap-4">
-            <IngrediantItem>Chicken</IngrediantItem>
-            <IngrediantItem>Duck</IngrediantItem>
-            <IngrediantItem>Sandwitch</IngrediantItem>
-            <IngrediantItem>Shawarma</IngrediantItem>
-            <IngrediantItem>Grill</IngrediantItem>
-            <IngrediantItem>Fish</IngrediantItem>
-            <IngrediantItem>Duck</IngrediantItem>
-            <IngrediantItem>Chicken</IngrediantItem>
-            <IngrediantItem>Duck</IngrediantItem>
+        <div class="grid grid-cols-2 sm:grid-cols-4 xl:grid-cols-6 grid-wrap gap-2 md:gap-4 my-2 md:my-4">
+            <template v-if="ingrediants.length > 0">
+                <IngrediantItem v-for="ingrediant of ingrediants" :key="ingrediant?.idIngredient">{{
+                    ingrediant.strIngredient }}
+                </IngrediantItem>
+            </template>
+            <template v-else class="text-center font-bold text-md">Loading...</template>
 
         </div>
     </div>
 </template>
 
 <script setup>
+import { onMounted, ref } from "vue";
 import IngrediantItem from "../components/IngrediantItem.vue";
+import axiosInstance from "../utils/axios";
+
+const ingrediants = ref([]);
+onMounted(() => {
+    axiosInstance.get("list.php?i=list").then(({ data }) => {
+        ingrediants.value = data.meals;
+    })
+})
 </script>
 
 <style scoped></style>
